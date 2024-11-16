9f3eb5a4c801e5dc9dbf493ac8ac8598dd2acd39
zihad868 <zihadh868@gmail.com>
2024-11-16 20:27:07 +0600

    Project & Project Members Models

--- projects/admin.py
+++ projects/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from . models import Projects, ProjectMembers
 
-# Register your models here.
+admin.site.register(Projects)
+admin.site.register(ProjectMembers)
--- projects/models.py
+++ projects/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.conf import settings
+from random import choices
 
-# Create your models here.
+class Projects(models.Model):
+    name = models.CharField(max_length=250)
+    description = models.TextField()
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='owned_project')
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.name
+    
+class ProjectMembers(models.Model):
+    ROLE_CHOICES = [
+        ('Admin', 'Admin'),
+        ('Member', 'Member')
+    ]
+    project = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='project_membership')
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='member')
+    role = models.CharField(max_length=30, choices=ROLE_CHOICES)
+    
+    def __str__(self):
+        return f'{self.user.email} - {self.project.name} - ({self.role})'

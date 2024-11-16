9174501412a4cfa130075f862ac03efe117ac41f
zihad868 <zihadh868@gmail.com>
2024-11-16 21:40:26 +0600

    Project Admin

--- projects/models.py
+++ projects/models.py
@@ -14,11 +14,19 @@ class Projects(models.Model):
 class ProjectMembers(models.Model):
     ROLE_CHOICES = [
         ('Admin', 'Admin'),
-        ('Member', 'Member')
+        ('Member', 'Member'),
     ]
-    project = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='project_membership')
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='member')
+    project = models.ForeignKey(
+        'projects.Projects',  # Reference the Projects model
+        on_delete=models.CASCADE,
+        related_name='project_membership'
+    )
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='member'
+    )
     role = models.CharField(max_length=30, choices=ROLE_CHOICES)
-    
+
     def __str__(self):
-        return f'{self.user.email} - {self.project.name} - ({self.role})'
+        return f"{self.project.name} - {self.user.email} ({self.role})"

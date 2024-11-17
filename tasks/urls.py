7c5702652dd013060be80fbda4621c233ce6cbef
zihad868 <zihadh868@gmail.com>
2024-11-17 16:32:45 +0600

    Task Update Delete

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from . views import TaskListCreateView
+from . views import TaskListCreateView, TaskDetailView
 
 urlpatterns = [
     path('api/projects/<int:project_id>/tasks/', TaskListCreateView.as_view(), name='task-list-create'),
+    path('api/tasks/<int:id>/', TaskDetailView.as_view(), name='task-detail'),
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -10,7 +10,7 @@ from . serializers import TaskSerializer, TaskCrateSerializer
 
 
 class TaskListCreateView(APIView):
-    permission_classes = [IsAuthenticated]
+    # permission_classes = [IsAuthenticated]
     
     
     def get(self, request, project_id, *args, **kwargs):
@@ -29,3 +29,32 @@ class TaskListCreateView(APIView):
         
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
+
+class TaskDetailView(APIView):
+    # permission_classes = [IsAuthenticated]
+
+    def get(self, request, id, *args, **kwargs):
+        task = get_object_or_404(Task, id=id)
+        serializer = TaskSerializer(task)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def put(self, request, id, *args, **kwargs):
+        task = get_object_or_404(Task, id=id)
+        serializer = TaskCrateSerializer(task, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def patch(self, request, id, *args, **kwargs):
+        task = get_object_or_404(Task, id=id)
+        serializer = TaskCrateSerializer(task, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, id, *args, **kwargs):
+        task = get_object_or_404(Task, id=id)
+        task.delete()
+        return Response({"message": "Task deleted successfully."}, status=status.HTTP_204_NO_CONTENT)
